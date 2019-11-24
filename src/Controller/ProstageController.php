4d460dda21d75ecda4212d5286b8b7087d517b0e
Miloud Bouazza <miloud.bouazza213@gmail.com>
2019-11-24 14:28:11 +0100

    affichage des page entreprises et formation

--- src/Controller/ProstageController.php
+++ src/Controller/ProstageController.php
@@ -12,4 +12,18 @@ class ProstageController extends AbstractController
     {
         return $this->render('prostage/index.html.twig');
     }
+    /**
+    * @Route("/entreprises", name="Prostages_entreprises")
+    */
+    public function afficherEntreprise()
+    {
+          return $this->render('prostage/affichageEntreprise.html.twig');
+    }
+    /**
+    * @Route("/formations", name="Prostages_formations")
+    */
+    public function afficherFormation()
+    {
+          return $this->render('prostage/affichageFormations.html.twig');
+    }
 }
